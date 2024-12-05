c667f0b466de4cb61e5724bac67a759499940369
saad <saadrhihil06@gmail.com>
2024-12-05 17:14:25 +0100

    commit math 1 saad

---
+++ epreuves_mathematiques.py
@@ -0,0 +1,13 @@
+import random
+def factorielle(n):
+    if n == 0 :
+     return 1
+    resultat = 1
+    for i in range(1, n+1):
+        resultat *= i
+    return resultat
+
+def epreuve_math_factorielle():
+
+
+
