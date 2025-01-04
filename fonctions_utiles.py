bdb5c0548e3b444ad6231db011d3b6fd08ebf3dc
louka <louka.perso@gmail.com>
2025-01-04 14:12:12 +0100

    Commit par Louka test

--- fonctions_utiles.py
+++ fonctions_utiles.py
@@ -6,7 +6,6 @@ def introduction():
     print("Règles du jeu :")
     print("1. Vous devez accomplir des épreuves pour gagner des clés.")
     print("2. L'objectif est de ramasser trois clés pour accéder à la salle du trésor.")
-    print("Bonne chance à tous les participants !")
     print("=" * 50)
 
 
@@ -17,9 +16,9 @@ def composer_equipe():
             nb_joueurs = int(input("Combien de joueurs voulez-vous inscrire dans l'équipe ? (max 3) : "))
             if 1 <= nb_joueurs <= 3:
                 break
-            print("Erreur : L'équipe doit comprendre entre 1 et 3 joueurs.")
+            print("Erreur : L'équipe doit comprendre entre 1 et 3 joueurs.") #test pour avoir le bon nombre de joeurs
         except ValueError:
-            print("Veuillez entrer un nombre valide.")
+            print("Veuillez entrer un nombre valide.") #test pour avoir un nb ou valeur valide
 
     for i in range(nb_joueurs):
         print(f"\nInscription du joueur {i + 1} :")
@@ -35,7 +34,7 @@ def composer_equipe():
         equipe.append(joueur)
 
     if not any(joueur["leader"] for joueur in equipe):
-        print("Aucun leader désigné. Le premier joueur devient automatiquement le leader.")
+        print("Aucun leader désigné. Le premier joueur devient automatiquement le leader.") #test si aucun joeur est leader
         equipe[0]["leader"] = True
 
     return equipe
@@ -54,7 +53,7 @@ def menu_epreuves():
             if 1 <= choix <= 4:
                 return choix
             print("Erreur : Veuillez choisir un numéro entre 1 et 4.")
-        except ValueError:
+        except ValueError: #test nb invalide
             print("Veuillez entrer un nombre valide.")
 
 
@@ -76,19 +75,16 @@ def choisir_joueur(equipe):
 
 def enregistrer_historique(joueur, epreuve, resultat):
 
-    ##Enregistre l'historique des épreuves dans un fichier texte.
-    historique_dir = "output"
+    ##création de historique
+    historique_dir = "historique"
     historique_file = os.path.join(historique_dir, "historique.txt")
-
     # Créer le répertoire  sortie si nécessaire
     if not os.path.exists(historique_dir):
         os.makedirs(historique_dir)
-
     with open(historique_file, 'a', encoding='utf-8') as fichier:
         fichier.write(f"Joueur : {joueur['nom']} ({joueur['profession']})\n")
         fichier.write(f"Épreuve : {epreuve}\n")
         fichier.write(f"Résultat : {resultat}\n")
         fichier.write(f"Clés gagnées : {joueur['cles_gagnees']}\n")
-        fichier.write("=" * 50 + "\n")
     print("Historique mis à jour.")
 #composer_equipe()
